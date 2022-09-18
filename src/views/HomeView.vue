264f70a0d3b70682ab29a10833d8b3f83ab11aeb
Youssef BAHI <you97ssef@gmail.com>
2022-09-18 14:37:56 +0200

    cleaned the app

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Chest application</h1>
   </main>
 </template>
