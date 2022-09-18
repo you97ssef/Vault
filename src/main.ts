6fdd32ddfa23394aec2d2a631b1a2e5a9a403aad
Youssef BAHI <you97ssef@gmail.com>
2022-09-19 00:49:54 +0200

    configured the app

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import type { Config } from "./models/config";
+import router from "./router";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
+app.use(router);
 
-app.mount('#app')
+const config: Config = { pass: "Secret", pin: null };
+app.config.globalProperties.config = config;
+
+app.mount("#app");
