e69f7986e818bc4fde6ccd01a5847a5a7ee0ba51
Youssef BAHI <you97ssef@gmail.com>
2022-09-24 00:08:15 +0200

    adding secret component and showing passwords

---
+++ src/components/Secret.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h5>{{ secret.id }}- {{ secret.topic }}</h5>
+        <p>Username: {{ secretService.decrypt(secret.username) }}</p>
+        <p>Secret: {{ secretService.decrypt(secret.secret) }}</p>
+    </div>
+</template>
+
+<script lang="ts">
+import { SecretService } from "@/services/secret-service";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    props:[
+        "secret"
+    ],
+    data() {
+        return {
+            secretService: new SecretService(this.config),
+        };
+    },
+});
+</script>
