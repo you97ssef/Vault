a53e0f25f52465618fbbcf1c233612c6011bcb3a
Youssef BAHI <you97ssef@gmail.com>
2022-10-09 23:48:59 +0200

    working on the secret card

--- src/components/Secret.vue
+++ src/components/Secret.vue
@@ -1,8 +1,32 @@
 <template>
-    <div>
-        <h5>{{ secret.id }}- {{ secret.topic }}</h5>
-        <p>Username: {{ secretService.decrypt(secret.username) }}</p>
-        <p>Secret: {{ secretService.decrypt(secret.secret) }}</p>
+    <div class="card m-5">
+        <div class="card-content">
+            <div class="content">
+                <p class="title is-3 has-text-centered">{{ secret.topic }}</p>
+                <div class="columns">
+                    <div class="column p-1 is-narrow">
+                        <button class="button is-light is-small" @click="showHideUsername()">{{ visibleUsername ? 'Hide' : 'Show' }}</button> 
+                    </div>
+                    <div class="column p-1">
+                        <p class="subtitle is-6 pt-1">
+                            <strong>Username:</strong> 
+                            {{ secret.username }} 
+                        </p>
+                    </div>
+                </div>
+                <div class="columns">
+                    <div class="column p-1 is-narrow">
+                        <button class="button is-light is-small" @click="showHideSecret()">{{ visibleSecret ? 'Hide' : 'Show' }}</button> 
+                    </div>
+                    <div class="column p-1">
+                        <p class="subtitle is-6 pt-1">
+                            <strong>Secret:</strong> 
+                            {{ secret.secret }} 
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -17,7 +41,25 @@ export default defineComponent({
     data() {
         return {
             secretService: new SecretService(this.config),
+            visibleUsername: false,
+            visibleSecret: false
         };
+    },
+    methods: {
+        showHideUsername() {
+            if(!this.visibleUsername)
+                this.secret.username = this.secretService.decrypt(this.secret.username)
+            else
+                this.secret.username = this.secretService.encrypt(this.secret.username)
+            this.visibleUsername = ! this.visibleUsername
+        },
+        showHideSecret() {
+            if(!this.visibleSecret)
+                this.secret.secret = this.secretService.decrypt(this.secret.secret)
+            else
+                this.secret.secret = this.secretService.encrypt(this.secret.secret)
+            this.visibleSecret = ! this.visibleSecret
+        }
     },
 });
 </script>
