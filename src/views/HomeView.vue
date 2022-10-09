a0ab9f908f7c573060247d375cac926873cd4e89
Youssef BAHI <you97ssef@gmail.com>
2022-10-09 22:38:08 +0200

    fixed key for key

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -18,12 +18,12 @@ export default defineComponent({
         <h1>Chest application</h1>
         <h3>Categories</h3>
         <ul>
-            <li v-for="cat of data.categories">
+            <li v-for="cat of data.categories" v-bind:key="cat.id">
                 <h5>{{ cat.id }}- {{ cat.name }}</h5>
                 <p>{{ cat.description }}</p>
                 <h6>Secrets</h6>
                 <ul>
-                    <li v-for="sec of cat.secrets">
+                    <li v-for="sec of cat.secrets" v-bind:key="sec.id">
                         <Secret :secret="sec"></Secret>
                     </li>
                 </ul>
