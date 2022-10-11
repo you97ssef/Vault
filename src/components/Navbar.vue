84b65d4f9d2453f73568641f1e2e8121a47ba52f
Youssef BAHI <you97ssef@gmail.com>
2022-10-12 00:01:35 +0200

    done the navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+    <nav class="bottom-border is-flex has-background-link-light is-justify-content-center">
+        <h1 class="p-5 title is-1">C H E S T</h1>
+    </nav>
+</template>
+
+<style>
+.bottom-border {
+  border-bottom: .2em black solid;
+}
+</style>
