2104a60e8fb6e469b44058c9f55b9e06c037e787
Youssef BAHI <you97ssef@gmail.com>
2022-10-12 00:30:31 +0200

    working on the secret component

--- src/components/Secret.vue
+++ src/components/Secret.vue
@@ -1,34 +1,53 @@
 <template>
-    <div class="card m-5">
+    <div class="card has-background-link-light">
         <div class="card-content">
-            <div class="content">
-                <p class="title is-3 has-text-centered">{{ secret.topic }}</p>
-                <div class="columns">
-                    <div class="column p-1 is-narrow">
-                        <button class="button is-light is-small" @click="showHideUsername()">{{ visibleUsername ? 'Hide' : 'Show' }}</button> 
-                    </div>
-                    <div class="column p-1">
-                        <p class="subtitle is-6 pt-1">
-                            <strong>Username:</strong> 
-                            {{ secret.username }} 
-                        </p>
-                    </div>
-                </div>
-                <div class="columns">
-                    <div class="column p-1 is-narrow">
-                        <button class="button is-light is-small" @click="showHideSecret()">{{ visibleSecret ? 'Hide' : 'Show' }}</button> 
-                    </div>
-                    <div class="column p-1">
-                        <p class="subtitle is-6 pt-1">
-                            <strong>Secret:</strong> 
-                            {{ secret.secret }} 
-                        </p>
-                    </div>
-                </div>
+            <p class="title is-2 has-text-centered mb-5">{{ secret.topic }}</p>
+            <p class="title is-6 has-text-centered mb-2">
+                <i class="fa-solid fa-user"></i>
+                {{ visibleUsername ? show(secret.username) : '************************' }}
+            </p>
+            <div class="buttons is-justify-content-center are-small">
+                <button class="button is-rounded is-black" @click="showHideUsername()">
+                    <i class="fa-solid fa-eye" v-if="!visibleUsername"></i>
+                    <i class="fa-solid fa-eye-slash" v-if="visibleUsername"></i>
+                </button> 
+                <button class="button is-rounded is-dark" @click="copy(show(secret.username))">
+                    <i class="fa-solid fa-copy"></i>
+                </button>
+            </div>
+            <p class="title is-6 has-text-centered mb-2">
+                <i class="fa-solid fa-key"></i>
+                {{ visibleSecret ? show(secret.secret) : '************************' }}
+            </p>
+            <div class="buttons is-justify-content-center are-small">
+                <button class="button is-rounded is-black" @click="showHideSecret()">
+                    <i class="fa-solid fa-eye" v-if="!visibleSecret"></i>
+                    <i class="fa-solid fa-eye-slash" v-if="visibleSecret"></i>
+                </button> 
+                <button class="button is-rounded is-dark" @click="copy(show(secret.secret))">
+                    <i class="fa-solid fa-copy"></i>
+                </button>
+            </div>
+            <div class="buttons is-justify-content-space-between">
+                <button type="button" class="borderless-button has-background-link-light">
+                    <i class="fa-solid fa-pen-to-square"></i>
+                </button>
+                <button type="button" class="borderless-button has-background-link-light">
+                    <i class="fa-solid fa-trash"></i>
+                </button>
             </div>
         </div>
     </div>
 </template>
+
+<style scoped>
+.card {
+    border: .2em black solid;
+}
+.borderless-button {
+    border: 0;
+}
+</style>
 
 <script lang="ts">
 import { SecretService } from "@/services/secret-service";
@@ -46,19 +65,18 @@ export default defineComponent({
         };
     },
     methods: {
+        show(secret: string){
+            return this.secretService.decrypt(secret)
+        },
         showHideUsername() {
-            if(!this.visibleUsername)
-                this.secret.username = this.secretService.decrypt(this.secret.username)
-            else
-                this.secret.username = this.secretService.encrypt(this.secret.username)
             this.visibleUsername = ! this.visibleUsername
         },
         showHideSecret() {
-            if(!this.visibleSecret)
-                this.secret.secret = this.secretService.decrypt(this.secret.secret)
-            else
-                this.secret.secret = this.secretService.encrypt(this.secret.secret)
             this.visibleSecret = ! this.visibleSecret
+        },
+        copy(string: string) {
+            navigator.clipboard.writeText(string);
+            console.log('copied successfully!')
         }
     },
 });
