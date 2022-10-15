9daf932e2bfb2eb38d97868670e7d8f2ba4ebd51
Youssef BAHI <you97ssef@gmail.com>
2022-10-15 15:52:39 +0200

    changed navbar title to a link

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
     <nav class="bottom-border is-flex has-background-link-light is-justify-content-center">
-        <h1 class="p-5 title is-1">C H E S T</h1>
+      <router-link class="p-5 title is-1" to="/">C H E S T</router-link>
     </nav>
 </template>
 
