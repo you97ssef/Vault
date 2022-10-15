d3c13037924ab65308946d7fb8dd3dd779e739e7
Youssef BAHI <you97ssef@gmail.com>
2022-10-15 14:12:56 +0200

    added a category card

---
+++ src/components/Category.vue
@@ -0,0 +1,28 @@
+<template>
+    <router-link :to="{ name: 'category', params: { id: category.id }}">
+        <div class="card has-background-link-light">
+            <div class="card-content">
+                <p class="subtitle is-3 has-text-centered m-3">{{ category.name }}</p>
+            </div>
+        </div>
+    </router-link>
+</template>
+
+<style scoped>
+.card {
+    border: .2em black solid;
+}
+.borderless-button {
+    border: 0;
+}
+</style>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    props:[
+        "category"
+    ],
+});
+</script>
