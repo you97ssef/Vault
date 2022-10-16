febc73f4001917ace59441c51aed2790438a76bc
Youssef BAHI <you97ssef@gmail.com>
2022-10-17 00:06:27 +0200

    added the category service with its todos

---
+++ src/data/category-service.ts
@@ -0,0 +1,18 @@
+import type { Category } from "@/models/category";
+
+export class CategoryService {
+    // TODO get category by id
+    get(categoryId: number) {}
+
+    // TODO new category
+    new(category: Category) {}
+
+    // TODO update category
+    update(categoryId: number, newCategory: Category) {}
+
+    // TODO delete category
+    delete(categoryId: number) {}
+
+    // TODO all the categories
+    all() {}
+}
