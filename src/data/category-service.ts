09c332ea8da171de728e00053ec81757ab7546db
Youssef BAHI <you97ssef@gmail.com>
2022-10-18 00:13:33 +0200

    done category service code needs to be tested

--- src/data/category-service.ts
+++ src/data/category-service.ts
@@ -1,18 +1,50 @@
 import type { Category } from "@/models/category";
 
 export class CategoryService {
-    // TODO get category by id
-    get(categoryId: number) {}
+    get(categoryId: number) {
+        let category = localStorage.getItem("category-" + categoryId);
 
-    // TODO new category
-    new(category: Category) {}
+        if (category) return JSON.parse(category);
 
-    // TODO update category
-    update(categoryId: number, newCategory: Category) {}
+        return null;
+    }
 
-    // TODO delete category
-    delete(categoryId: number) {}
+    new(category: Category) {
+        let count = JSON.parse(localStorage.getItem("count")!);
 
-    // TODO all the categories
-    all() {}
+        category.id = count.categories = parseInt(count.categories) + 1;
+        localStorage.setItem("count", JSON.stringify(count));
+
+        localStorage.setItem("category-" + category.id, JSON.stringify(category));
+    }
+
+    update(categoryId: number, newCategory: Category) {
+        let category = this.get(categoryId);
+
+        if (category) {
+            newCategory.id = categoryId;
+
+            localStorage.setItem("category-" + category.id, JSON.stringify(category));
+        }
+    }
+
+    delete(categoryId: number) {
+        let category = this.get(categoryId);
+
+        if (category) localStorage.removeItem("category-" + category.id);
+    }
+
+    all() {
+        let count = JSON.parse(localStorage.getItem("count")!);
+
+        let categories = [];
+
+        for (let i = 1; i <= parseInt(count.categories); i++) {
+            let category = this.get(i);
+
+            if (category) categories.push(category);
+        }
+
+        return categories;
+    }
 }
--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,7 +1,7 @@
 import type { Secret } from "./secret";
 
 export interface Category {
-    id: number;
+    id: number | null;
     name: string;
     description: string | null;
     secrets: Secret[];
