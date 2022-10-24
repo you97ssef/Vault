23fc4a59d29bee9e155c278bc1c0421663268d27
Youssef BAHI <you97ssef@gmail.com>
2022-10-25 00:06:27 +0200

    done the modal of the app

---
+++ src/components/Modal.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="modal"
+        :class="state.active"
+    >
+        <div class="modal-background" @click="close()"></div>
+        <div class="modal-card">
+            <header class="modal-card-head">
+                <p class="modal-card-title">{{ state.title }}</p>
+                <button class="delete" @click="close()"></button>
+            </header>
+            <section class="modal-card-body">
+                <p>Are you sure you want to make this change ?</p>
+            </section>
+            <footer class="modal-card-foot">
+                <button class="button is-dark" @click="submit()">{{ state.button }}</button>
+                <button class="button" @click="close()">Cancel</button>
+            </footer>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    props:["state"],
+    methods: {
+        close() {
+            this.$emit("close", "")
+        },
+        submit() {
+            this.$emit("submit", this.state.button)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
