40408310756f5004195c43799e715633f180fad5
Youssef BAHI <you97ssef@gmail.com>
2022-11-03 00:03:00 +0100

    changed chest to vault

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
     <nav class="bottom-border is-flex has-background-link-light is-justify-content-center">
-      <router-link class="p-5 title is-1" to="/">C H E S T</router-link>
+      <router-link class="p-5 title is-1" to="/">V A U L T</router-link>
     </nav>
 </template>
 
