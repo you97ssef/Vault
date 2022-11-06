d58b1b0c70938fcc2c2bb0d6ec63ca149092ffd1
Youssef BAHI <you97ssef@gmail.com>
2022-11-06 13:21:45 +0100

    added routes guards

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,33 @@
 import Categories from '@/views/category/Categories.vue'
 import CategoryEdition from '@/views/category/CategoryEdition.vue'
+
 import Secrets from '@/views/secret/Secrets.vue'
-import NotFound from '@/views/NotFound.vue'
-import Setup from '@/views/Setup.vue'
-import { createRouter, createWebHistory } from 'vue-router'
 import SecretEdition from '@/views/secret/SecretEdition.vue'
+
+import Setup from '@/views/Setup.vue'
 import Home from '@/views/Home.vue'
+
+import NotFound from '@/views/NotFound.vue'
+
+import { createRouter, createWebHistory } from 'vue-router'
+import store from "@/store/index"
+
+
+function setUp(to: any, from: any, next: any) {
+  if (store.state.code) {
+      next();
+  } else {
+      next("/setup");
+  }
+}
+
+function unSet(to: any, from: any, next: any) {
+  if (!store.state.code) {
+      next();
+  } else {
+      next("/categories");
+  }
+}
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,7 +35,8 @@ const router = createRouter({
     {
       path: '/setup',
       name: 'setup',
-      component: Setup
+      component: Setup,
+      beforeEnter: unSet
     },
     {
       path: '/',
@@ -23,32 +46,38 @@ const router = createRouter({
     {
       path: '/categories',
       name: 'categories',
-      redirect: '/'
+      component: Categories,
+      beforeEnter: setUp
     },
     {
       path: '/categories/:id',
       name: 'category',
-      component: Secrets
+      component: Secrets,
+      beforeEnter: setUp
     },
     {
       path: '/categories/:id/edit',
       name: 'edit-category',
-      component: CategoryEdition
+      component: CategoryEdition,
+      beforeEnter: setUp
     },
     {
       path: '/new-category',
       name: 'new-category',
-      component: CategoryEdition
+      component: CategoryEdition,
+      beforeEnter: setUp
     },
     {
       path: '/categories/:categoryId/new-secret',
       name: 'new-secret',
-      component: SecretEdition
+      component: SecretEdition,
+      beforeEnter: setUp
     },
     {
       path: '/categories/:categoryId/edit-secret/:secretId',
       name: 'edit-secret',
-      component: SecretEdition
+      component: SecretEdition,
+      beforeEnter: setUp
     },
     {
       path: '/404',
