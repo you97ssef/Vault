d41d81c17d08a51594fccdc52e24041127132bb5
Youssef BAHI <you97ssef@gmail.com>
2022-11-06 14:01:46 +0100

    added categories

--- src/components/UnsetCode.vue
+++ src/components/UnsetCode.vue
@@ -1,6 +1,10 @@
 <template>
-    <div class="is-flex is-justify-content-center" v-if="codeIsSet">
-        <button class="button is-rounded is-dark is-small" @click="unsetCode()">
+    <div class="is-flex is-justify-content-center buttons" v-if="codeIsSet">
+        <router-link class="button is-rounded is-dark is-small" to="/categories">
+            <span class="icon ml-1"><i class="fa-solid fa-layer-group fa-lg"></i></span>
+            <span>Categories</span>
+        </router-link>
+        <button class="button is-rounded is-light is-small" @click="unsetCode()">
             <span class="icon ml-1"><i class="fa-solid fa-arrow-right-from-bracket fa-lg"></i></span>
             <span>Unset the code</span>
         </button>
