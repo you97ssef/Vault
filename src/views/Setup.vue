bb06ad04ed3a62b863bf67b37b3e8c5089dc2a2d
Youssef BAHI <you97ssef@gmail.com>
2022-11-06 13:23:25 +0100

    done the setup of the app code

--- src/views/Setup.vue
+++ src/views/Setup.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="has-text-centered top-space">
         <img src="@/assets/images/secret.svg" alt="empty" width="200">
-        <form class="box mt-5">
+        <form class="box mt-5" @submit.prevent="setSecret()">
             <h3 class="title is-3">Setup your secret code</h3>
             <h6 class="subtitle is-6">The code that will be used to hash your secrets.</h6>
             <div class="field">
                 <div class="control">
-                    <input class="input" type="password" placeholder="********">
+                    <input class="input" type="password" v-model="code" placeholder="********" required>
                 </div>
             </div>
 
@@ -20,7 +20,17 @@
 
 <script lang="ts">
 export default {
-
+    methods: {
+        setSecret() {
+            this.$store.commit("SET_CODE", this.code);
+            this.$router.push("/categories")
+        }
+    },
+    data() {
+        return {
+            code: null
+        }
+    }
 }
 </script>
 
