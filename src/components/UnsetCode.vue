06e481ff1c7f4bf044c1c131795697853c995128
Youssef BAHI <you97ssef@gmail.com>
2022-11-06 13:22:59 +0100

    added component to unset the user code

---
+++ src/components/UnsetCode.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="is-flex is-justify-content-center" v-if="codeIsSet">
+        <button class="button is-rounded is-dark is-small" @click="unsetCode()">
+            <span class="icon ml-1"><i class="fa-solid fa-arrow-right-from-bracket fa-lg"></i></span>
+            <span>Unset the code</span>
+        </button>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    methods: {
+        unsetCode() {
+            this.$store.commit("SET_CODE", null);
+            this.$router.push("/setup");
+        }
+    },
+    computed: {
+        codeIsSet() {
+            return this.$store.getters.GET_CODE
+        },
+    },
+}
+</script>
+
+<style scoped>
+</style>
