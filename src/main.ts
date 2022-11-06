aaf61467d5f2b1bec9fc6737812f9a22b83fb3be
Youssef BAHI <you97ssef@gmail.com>
2022-11-06 22:10:06 +0100

    added app footer with import export functionalities

--- src/main.ts
+++ src/main.ts
@@ -9,11 +9,14 @@ import "../node_modules/@fortawesome/fontawesome-free/css/all.min.css";
 import { CategoryRepo } from "./data/category-repo";
 import { SecretRepo } from "./data/secret-repo";
 
+import { DataService } from "./services/data-service";
+
 const app = createApp(App).use(store);
 
 app.use(router);
 app.use(store);
 
+app.config.globalProperties.dataService = new DataService();
 app.config.globalProperties.categoryRepo = new CategoryRepo();
 app.config.globalProperties.secretRepo = new SecretRepo();
 
